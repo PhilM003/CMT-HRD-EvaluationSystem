9e147f26bc13522c8fc861fba4b417797b0cb6ce
PhilM003 <burin.wo@gmail.com>
2025-12-09 16:58:12 +0700

    New

--- src/App.jsx
+++ src/App.jsx
@@ -549,6 +549,7 @@ const EvaluationForm = ({ initialData, employeeList = [], currentRole, onBack, o
   const isAdmin = currentRole === 'admin';
   const canEdit = (targetRole) => {
       if (status === 'completed' || status === 'rejected') return false;
+      if (targetRole === 'general') targetRole = 'assessor';
       if (isAdmin && targetRole === 'assessor') return true;
       return currentRole === targetRole;
     };
@@ -636,7 +637,7 @@ const EvaluationForm = ({ initialData, employeeList = [], currentRole, onBack, o
 
   const handleInputChange = (e) => {
     const { name } = e.target;
-    if (name !== 'employeeName' && currentRole !== 'admin') return; 
+    if (name !== 'employeeName' && currentRole !== 'admin' && currentRole !== 'assessor') return; 
 
     const { value, type, checked } = e.target;
     if (name.includes('.')) {
@@ -1736,7 +1737,7 @@ const htmlContent = `
                 min-height: 297mm;
                 margin: 0 auto;
                 background: white;
-                padding: 10mm; /* ระยะขอบกระดาษขาว */
+                padding: 5mm; /* ระยะขอบกระดาษขาว */
                 display: flex;
                 flex-direction: column;
             }
@@ -1785,6 +1786,10 @@ const htmlContent = `
             table.eval-table { width: 100%; border-collapse: collapse; margin-top: 15px; border: 1px solid #000; }
             table.eval-table th, table.eval-table td { border: 1px solid #000; padding: 4px; vertical-align: middle; }
             table.eval-table th { background-color: #f0f0f0; font-weight: bold; text-align: center; height: 40px; }
+
+            table.head-eval-table { width: 100%; table-layout: fixed; border-collapse: collapse; margin-top: 15px; border: 1px solid #000; }
+            table.head-eval-table th, table.head-eval-table td { border: 1px solid #000; padding: 4px; vertical-align: middle; }
+            table.head-eval-table th { background-color: #f0f0f0; font-weight: bold; text-align: center; height: 40px; }
             
             .diagonal-cell { position: relative; width: 70px; padding: 0 !important; background: linear-gradient(to top right, transparent 48%, #000 49%, #000 51%, transparent 52%); }
             .diag-top { position: absolute; top: 2px; right: 2px; font-size: 10px; text-align: right; line-height: 1; }
@@ -1821,13 +1826,13 @@ const htmlContent = `
                 
                 <div class="container">
                     <header class="header">
-                        <div class="company-info">
+                        <div class="company-info" style = "width: 40%;">
                             <div><strong>บริษัท คาร์เปท เมกเกอร์ (ประเทศไทย) จำกัด และ</strong></div>
                             <div><strong>บริษัท คาร์เปท เมกเกอร์ พี2ดับบลิว (ประเทศไทย) จำกัด</strong></div>
                             <div style="margin-top:2px;">The Carpet Maker (Thailand) Ltd. And</div>
                             <div>The Carpet Maker P2W (Thailand) Ltd.</div>
                         </div>
-                        <div class="form-title">
+                        <div class="form-title" style = "width: 60%; text-align: center;">
                             <h1>แบบประเมินผลการทดลองงานของพนักงาน</h1>
                             <p>Probation Evaluation Form</p>
                         </div>
@@ -1846,13 +1851,13 @@ const htmlContent = `
                         </div>
                         <div class="info-row">
                             <span class="field-label">เริ่มงานวันที่ (Start):</span> 
-                            <span class="field-value" style="width: 50px; flex:none;">${startD.d}</span>
-                            <span class="field-label">เดือน:</span> <span class="field-value" style="width: 80px; flex:none;">${startD.m}</span>
-                            <span class="field-label">พ.ศ.:</span> <span class="field-value" style="width: 50px; flex:none;">${startD.y}</span>
+                            <span class="field-value" style="width: 20px; flex:none;">${startD.d}</span>
+                            <span class="field-label">เดือน:</span> <span class="field-value" style="width: 75px; flex:none;">${startD.m}</span>
+                            <span class="field-label">พ.ศ.:</span> <span class="field-value" style="width: 35px; flex:none;">${startD.y}</span>
                             <span style="flex-grow:1;"></span> <span class="field-label">ครบกำหนด (Due):</span> 
-                            <span class="field-value" style="width: 50px; flex:none;">${dueD.d}</span>
-                            <span class="field-label">เดือน:</span> <span class="field-value" style="width: 80px; flex:none;">${dueD.m}</span>
-                            <span class="field-label">พ.ศ.:</span> <span class="field-value" style="width: 50px; flex:none;">${dueD.y}</span>
+                            <span class="field-value" style="width: 20px; flex:none;">${dueD.d}</span>
+                            <span class="field-label">เดือน:</span> <span class="field-value" style="width: 75px; flex:none;">${dueD.m}</span>
+                            <span class="field-label">พ.ศ.:</span> <span class="field-value" style="width: 35px; flex:none;">${dueD.y}</span>
                         </div>
                     </div>
 
@@ -1877,36 +1882,36 @@ const htmlContent = `
                         เขียนวงกลมล้อมรอบคะแนนที่ประเมินให้ (Write a circle around the rating that is evaluated)
                     </div>
 
-                    <table class="eval-table">
+                    <table class="head-eval-table">
                         <thead>
                             <tr>
-                                <th rowspan="2" style="width:35%; text-align:left;">หัวข้อในการประเมิน<br><span style="font-weight:normal; font-style:italic;">(Evaluate Topic)</span></th>
-                                <th rowspan="2" class="diagonal-cell">
-                                    <div class="diag-top">คะแนน<br>Score</div>
-                                    <div class="diag-bottom">น้ำหนัก<br>Weight</div>
+                                <th rowspan="2" style="width:52%; text-align:center;">หัวข้อในการประเมิน<br><span style="font-weight:normal; font-style:italic;">(Evaluate Topic)</span></th>
+                                <th rowspan="2" class="diagonal-cell" style="font-size: 4px; width: 6%">
+                                    <div class="diag-top" style="font-size: 6px;">คะแนน<br>Score</div>
+                                    <div class="diag-bottom" style="font-size: 6px;">น้ำหนัก<br>Weight</div>
                                 </th>
-                                <th>ใช้ไม่ได้<br>(Bad)<br>1</th>
-                                <th>ต้องปรับปรุง<br>(Poor)<br>2</th>
-                                <th>พอใช้<br>(Fair)<br>3</th>
-                                <th>ดี<br>(Good)<br>4</th>
-                                <th>ดีมาก<br>(Very Good)<br>5</th>
-                                <th>ดีเยี่ยม<br>(Excellent)<br>6</th>
-                                <th>ดีเลิศ<br>(Perfect)<br>7</th>
+                                <th style="font-size: 8px; width: 6%">ใช้ไม่ได้<br>(Bad)<br>1</th>
+                                <th style="font-size: 8px; width: 6%">ต้องปรับปรุง<br>(Poor)<br>2</th>
+                                <th style="font-size: 8px; width: 6%">พอใช้<br>(Fair)<br>3</th>
+                                <th style="font-size: 8px; width: 6%">ดี<br>(Good)<br>4</th>
+                                <th style="font-size: 8px; width: 6%">ดีมาก<br>(Very Good)<br>5</th>
+                                <th style="font-size: 7px; width: 6%">ดีเยี่ยม<br>(Excellent)<br>6</th>
+                                <th style="font-size: 8px; width: 6%">ดีเลิศ<br>(Perfect)<br>7</th>
                                 <th rowspan="2" style="width:8%;">คะแนน<br>(Score)</th>
                             </tr>
                         </thead>
                         <tbody>
                             ${[
-                                {id:1, w:15, t:'ปริมาณงานที่ทำสำเร็จ', e:'(The amount of work accomplished)'},
+                                {id:1, w:15, t:'ปริมาณงานที่ทำสำเร็จจากที่ได้มอบหมาย', e:'(The amount of work accomplished from the assignment)'},
                                 {id:2, w:15, t:'คุณภาพของงานที่ทำสำเร็จ', e:'(The quality of the complete work)'},
-                                {id:3, w:15, t:'การปฏิบัติตามคำสั่ง / WI / WP', e:'(Compliance with orders)'},
-                                {id:4, w:10, t:'ความสามารถการเรียนรู้งาน', e:'(Ability to learn)'},
-                                {id:5, w:10, t:'ความรับผิดชอบในงาน', e:'(Responsibility)'},
-                                {id:6, w:10, t:'ความร่วมมือในการทำงานเป็นทีม', e:'(Cooperation / Teamwork)'},
-                                {id:7, w:10, t:'การตรงต่อเวลา', e:'(Punctuality)'},
-                                {id:8, w:5,  t:'ความปลอดภัยและชีวอนามัย', e:'(Safety and Health)'},
-                                {id:9, w:5,  t:'ความซื่อสัตย์ / ทัศนคติ', e:'(Honesty / Attitude)'},
-                                {id:10, w:5, t:'การปฏิบัติตามกฎระเบียบ', e:'(Compliance with rules)'}
+                                {id:3, w:15, t:'การปฏิบัติตามคำสั่งของผู้บังคับบัญชาหรือตาม WI หรือ WP', e:'(Compliance with orders of supervisors or according to WI or WP)'},
+                                {id:4, w:10, t:'ความสามารถการเรียนรู้งานและความเข้าใจในงานที่ทำ', e:'(Ability to learn and understand the work)'},
+                                {id:5, w:10, t:'ความไว้วางใจและความรับผิดชอบในงานที่ได้รับมอบหมาย', e:'(Trust and responsibility in assigned work)'},
+                                {id:6, w:10, t:'ความร่วมมือในการทำงานเป็นทีม', e:'(Cooperation in Teamwork)'},
+                                {id:7, w:10, t:'การตรงต่อเวลาและความสม่ำเสมอในการมาทำงาน', e:'(Punctuality in working, quitting work and Consistency in working)'},
+                                {id:8, w:5,  t:'การเอาใจใส่ในการปฏิบัติตามกฎเรื่องความปลอดภัยและชีวอนามัยของบริษัทฯ', e:'(Caring for compliance the safety and health regulations of the company)'},
+                                {id:9, w:5,  t:'ความชื่อสัตย์ และทัศนคติที่ดีต่อบริษัท', e:'(Honesty and good attitude towards the company)'},
+                                {id:10, w:5, t:'การปฏิบัติตามกฎระเบียบและการรักษาทรัพย์สินของบริษัท', e:'(Compliance with rules and regulations for maintaining company assets)'}
                             ].map(topic => {
                                 const score = data.ratings[topic.id];
                                 const calcScore = score ? ((topic.w / 7) * score).toFixed(2) : '';
@@ -2002,7 +2007,7 @@ const htmlContent = `
                 </div>
 
             </div> </div> <script>
-            window.onload = function() { setTimeout(function(){ window.print(); }, 500); }
+            // window.onload = function() { setTimeout(function(){ window.print(); }, 500); }
         </script>
     </body>
     </html>
